c43f12b407c51d2d7d2da4030f84dc23f9fce44e
Luis Urraca <l.urraca@gmail.com>
2012-07-11 12:11:27 -0400

    fixing search issue

--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -6,17 +6,21 @@ class VisitorController < ApplicationController
 
   def shops
     @stores = Store.all
+    @search = Store.search(params[:search])
   end
 
   def shop
     @store = Store.find(params[:store])
+    @search = Store.search(params[:search])
   end
 
   def about
     @aboutvideo = Aboutvideo.all
+    @search = Store.search(params[:search])
   end
 
   def what
+    @search = Store.search(params[:search])
   end
 
   def blog
